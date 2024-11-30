9957b8a7bd6199576cefe118a4efdae89ca830f4
MrJuise <126503964+MrJuise@users.noreply.github.com>
2024-12-01 00:01:55 +0300

    Homework 3

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,4 +2,4 @@ from app.models.user import User
 from app.models.task import Task
 
 from sqlalchemy.schema import CreateTable
-print(CreateTable(User.__table__))
+print(CreateTable(Task.__table__))
